98a036a92353e153309381e482c34352cfaa0c0a
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-12 21:01:53 -0700

    added technologies section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0883c705ac4a90b0cccd"; }
+/******/ 		__webpack_require__.h = function() { return "04a4a307ea26024dfd76"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,10 +3,36 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>I've aquired a familiar understanding of many technologies used in the world of web development</SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front End</ListTitle>
+          <ListParagraph> Experience with <br />React, Next, and <br />Vite</ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph> Experience with <br />Node, MySql2, Sequelize, MongoDB, and Firebase</ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>Design</ListTitle>
+          <ListParagraph> Experience with <br />Tailwind, SCSS, and <br />Styled-Components</ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
